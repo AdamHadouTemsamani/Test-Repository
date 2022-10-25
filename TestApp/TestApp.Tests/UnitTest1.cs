23f1a3d54823e3d1d9031bd95d9f2fb5c6ae08f2
Adam Hadou Temsamani <ahad@itu.dk>
2022-10-25 21:22:57 +0200

    Added Xunit tests to Repository

--- TestApp/TestApp.Tests/UnitTest1.cs
+++ TestApp/TestApp.Tests/UnitTest1.cs
@@ -3,8 +3,11 @@ namespace TestApp.Tests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Simple_Test_That_Returns_Answer()
     {
+        Program app = new Program();
+        var answer = app.PrintOut("input");
 
+        Assert.Equal("input", answer);
     }
 }
