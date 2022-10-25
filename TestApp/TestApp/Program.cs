4761240a65bdbe8379612031f194c1fec7d650f1
Adam Hadou Temsamani <ahad@itu.dk>
2022-10-25 21:19:34 +0200

    Update Program.cs

--- TestApp/TestApp/Program.cs
+++ TestApp/TestApp/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+public class Program
+{
+    public static void Main(string args)
+    {
+        Console.WriteLine("This is a Test Repository");
+    }
+
+    public void PrintOut(string input)
+    {
+        Console.WriteLine(input);
+    }
+
+}
